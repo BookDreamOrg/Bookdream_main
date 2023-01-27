307c843e73adf894d6e704746b64362702c0ae04
vbvb127 <80655549+vbvb127@users.noreply.github.com>
2023-01-27 15:30:18 +0900

    Update bookdream_database.sql
    review 컬럼 변경 (user_no > user_id 변경)

--- database/bookdream_database.sql
+++ database/bookdream_database.sql
@@ -44,7 +44,7 @@ drop table review;
 
 create table review(
 review_no number(10) not null,
-USER_NO number(10) not null,
+USER_ID  varchar2(20)  not null,
 book_no number(10) not null,
 REVIEW_CONTENT varchar2(1000) not null,
 REVIEW_DATE date default sysdate,
